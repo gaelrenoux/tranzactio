06bf7aa921114472d23cffcb0f78199b9eb93af9
Gaël Renoux <gael.renoux@gmail.com>
2020-04-26 17:32:39 +0200

    Cleans up Samples test

--- src/samples/scala/samples/SamplesSpec.scala
+++ src/samples/scala/samples/SamplesSpec.scala
@@ -8,25 +8,14 @@ import zio.test.{DefaultRunnableSpec, ZSpec, _}
 object SamplesSpec extends DefaultRunnableSpec {
 
   override def spec: ZSpec[TestEnvironment, Any] = suite("SamplesSpec")(
+    testApp("Doobie", doobie.LayeredApp),
+    testApp("Anorm", anorm.LayeredApp)
+  )
 
-    testM("Doobie LayeredApp prints its progress then the trio") {
+  private def testApp(name: String, app: zio.App) =
+    testM(s"$name LayeredApp prints its progress then the trio") {
       for {
-        _ <- doobie.LayeredApp.run(Nil)
-        output <- TestConsole.output
-      } yield assert(output)(equalTo(Vector(
-        "Loading configuration\n",
-        "Setting up the env\n",
-        "Calling the app\n",
-        "Creating the table\n",
-        "Inserting the trio\n",
-        "Reading the trio\n",
-        "Buffy Summers, Willow Rosenberg, Alexander Harris\n"
-      )))
-    },
-
-    testM("Anorm LayeredApp prints its progress then the trio") {
-      for {
-        _ <- anorm.LayeredApp.run(Nil)
+        _ <- app.run(Nil)
         output <- TestConsole.output
       } yield assert(output)(equalTo(Vector(
         "Loading configuration\n",
@@ -38,7 +27,5 @@ object SamplesSpec extends DefaultRunnableSpec {
         "Buffy Summers, Willow Rosenberg, Alexander Harris\n"
       )))
     }
-
-  )
 
 }
