120c9aabc7c460bd87167c6e9d259697f735fd57
Gaël Renoux <gael.renoux@gmail.com>
2020-06-04 11:50:15 +0200

    More small cleanup on ZCatsBlocker

--- src/main/scala/io/github/gaelrenoux/tranzactio/utils/package.scala
+++ src/main/scala/io/github/gaelrenoux/tranzactio/utils/package.scala
@@ -6,8 +6,8 @@ import zio.blocking.Blocking
 
 package object utils {
 
-  val ZCatsBlocker: ZIO[Blocking, Nothing, Blocker] = ZIO
-    .access[Blocking](_.get.blockingExecutor.asEC)
-    .map(Blocker.liftExecutionContext)
+  val ZCatsBlocker: ZIO[Blocking, Nothing, Blocker] = ZIO.access[Blocking] { b =>
+    Blocker.liftExecutionContext(b.get.blockingExecutor.asEC)
+  }
 
 }
