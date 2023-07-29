457f621aee16a4b7644fd96be535d453b66f2017
Gaël Renoux <gael.renoux@gmail.com>
2023-07-29 14:48:55 +0200

    Bumps ZIO-Cats version

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import xerial.sbt.Sonatype.autoImport._
 object BuildHelper {
   object V {
     val zio = "2.0.13"
-    val zioCats = "23.0.0.5"
+    val zioCats = "23.0.0.8"
     val cats = "2.9.0"
     val doobie = "1.0.0-RC2"
     val anorm = "2.7.0"
