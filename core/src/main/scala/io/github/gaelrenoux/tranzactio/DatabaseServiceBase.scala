5a565728d430f703bdcb7af4e56cf8221596494e
Jules Ivanic <jules.ivanic@gmail.com>
2024-06-24 15:12:24 +1000

    Remove some unnecessary `.map`

--- core/src/main/scala/io/github/gaelrenoux/tranzactio/DatabaseServiceBase.scala
+++ core/src/main/scala/io/github/gaelrenoux/tranzactio/DatabaseServiceBase.scala
@@ -15,43 +15,33 @@ abstract class DatabaseServiceBase[Connection: Tag](connectionSource: Connection
   def connectionFromJdbc(connection: => JdbcConnection)(implicit trace: Trace): ZIO[Any, Nothing, Connection]
 
   override def transaction[R, E, A](zio: => ZIO[Connection with R, E, A], commitOnFailure: => Boolean = false)
-    (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZIO[R, Either[DbException, E], A] =
-    ZIO.environmentWithZIO[R] { r =>
-      runTransaction({ (c: JdbcConnection) =>
-        connectionFromJdbc(c)
-          .map(r ++ ZEnvironment(_))
-          .flatMap(zio.provideEnvironment(_))
-      }, commitOnFailure)
-    }
+                                   (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZIO[R, Either[DbException, E], A] =
+    runTransaction({ (c: JdbcConnection) =>
+      connectionFromJdbc(c)
+        .flatMap(connection => zio.provideSomeEnvironment[R](_ ++ ZEnvironment(connection)))
+    }, commitOnFailure)
 
   override def transactionOrDieStream[R, E, A](stream: => ZStream[Connection with R, E, A], commitOnFailure: => Boolean = false)
-    (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZStream[R, E, A] =
-    ZStream.environmentWithStream[R] { r =>
-      runTransactionOrDieStream({ (c: JdbcConnection) =>
-        ZStream.fromZIO(connectionFromJdbc(c))
-          .map(r ++ ZEnvironment(_))
-          .flatMap(stream.provideEnvironment(_))
-      }, commitOnFailure)
-    }
+                                              (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZStream[R, E, A] =
+    runTransactionOrDieStream({ (c: JdbcConnection) =>
+      ZStream
+        .fromZIO(connectionFromJdbc(c))
+        .flatMap(connection => stream.provideSomeEnvironment[R](_ ++ ZEnvironment(connection)))
+    }, commitOnFailure)
 
   override def autoCommit[R, E, A](zio: => ZIO[Connection with R, E, A])
-    (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZIO[R, Either[DbException, E], A] =
-    ZIO.environmentWithZIO[R] { r =>
-      runAutoCommit { (c: JdbcConnection) =>
-        connectionFromJdbc(c)
-          .map(r ++ ZEnvironment(_))
-          .flatMap(zio.provideEnvironment(_))
-      }
+                                  (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZIO[R, Either[DbException, E], A] =
+    runAutoCommit { (c: JdbcConnection) =>
+      connectionFromJdbc(c)
+        .flatMap(connection => zio.provideSomeEnvironment[R](_ ++ ZEnvironment(connection)))
     }
 
   override def autoCommitStream[R, E, A](stream: => ZStream[Connection with R, E, A])
-    (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZStream[R, Either[DbException, E], A] =
-    ZStream.environmentWithStream[R] { r =>
-      runAutoCommitStream { (c: JdbcConnection) =>
-        ZStream.fromZIO(connectionFromJdbc(c))
-          .map(r ++ ZEnvironment(_))
-          .flatMap(stream.provideEnvironment(_))
-      }
+                                        (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZStream[R, Either[DbException, E], A] =
+    runAutoCommitStream { (c: JdbcConnection) =>
+      ZStream
+        .fromZIO(connectionFromJdbc(c))
+        .flatMap(connection => stream.provideSomeEnvironment[R](_ ++ ZEnvironment(connection)))
     }
 
 }
