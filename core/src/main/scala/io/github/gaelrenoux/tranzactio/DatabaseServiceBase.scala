abb74f11fcc48c9e362c8762afce28ccba97a379
Gaël Renoux <gael.renoux@gmail.com>
2024-08-15 13:51:13 +0200

    Fix indentation

--- core/src/main/scala/io/github/gaelrenoux/tranzactio/DatabaseServiceBase.scala
+++ core/src/main/scala/io/github/gaelrenoux/tranzactio/DatabaseServiceBase.scala
@@ -15,14 +15,14 @@ abstract class DatabaseServiceBase[Connection: Tag](connectionSource: Connection
   def connectionFromJdbc(connection: => JdbcConnection)(implicit trace: Trace): ZIO[Any, Nothing, Connection]
 
   override def transaction[R, E, A](zio: => ZIO[Connection with R, E, A], commitOnFailure: => Boolean = false)
-                                   (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZIO[R, Either[DbException, E], A] =
+    (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZIO[R, Either[DbException, E], A] =
     runTransaction({ (c: JdbcConnection) =>
       connectionFromJdbc(c)
         .flatMap(connection => zio.provideSomeEnvironment[R](_ ++ ZEnvironment(connection)))
     }, commitOnFailure)
 
   override def transactionOrDieStream[R, E, A](stream: => ZStream[Connection with R, E, A], commitOnFailure: => Boolean = false)
-                                              (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZStream[R, E, A] =
+    (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZStream[R, E, A] =
     runTransactionOrDieStream({ (c: JdbcConnection) =>
       ZStream
         .fromZIO(connectionFromJdbc(c))
@@ -30,14 +30,14 @@ abstract class DatabaseServiceBase[Connection: Tag](connectionSource: Connection
     }, commitOnFailure)
 
   override def autoCommit[R, E, A](zio: => ZIO[Connection with R, E, A])
-                                  (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZIO[R, Either[DbException, E], A] =
+    (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZIO[R, Either[DbException, E], A] =
     runAutoCommit { (c: JdbcConnection) =>
       connectionFromJdbc(c)
         .flatMap(connection => zio.provideSomeEnvironment[R](_ ++ ZEnvironment(connection)))
     }
 
   override def autoCommitStream[R, E, A](stream: => ZStream[Connection with R, E, A])
-                                        (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZStream[R, Either[DbException, E], A] =
+    (implicit errorStrategies: ErrorStrategiesRef, trace: Trace): ZStream[R, Either[DbException, E], A] =
     runAutoCommitStream { (c: JdbcConnection) =>
       ZStream
         .fromZIO(connectionFromJdbc(c))
